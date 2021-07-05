69f36cd660631df2646e299c6df04792156262e7
Rafael Ambrosio <rafaelambrosio.mail@gmail.com>
2021-07-05 16:26:59 -0300

    alteração e correção da api

--- routes/produtos.js
+++ routes/produtos.js
@@ -1,25 +1,55 @@
 const express = require('express');
 const router = express.Router();
 
-// router.get('/', (req, res, next) => {
-//      res.status(200).send ({ 
-//          messagem: 'Usando GET dentro da rota de produtos'
-//      });
-//  });
+let db = [
+  {'1': {Nome: 'Cliente 1', Idade: '20'} },
+  {'2': {Nome: 'Cliente 2', Idade: '20'} },
+  {'3': {Nome: 'Cliente 3', Idade: '20'} }
+]
 
 router.get('/', (req, res, next) => {
-    res.status(200).send({
-        mensagem: 'Ok, Deu Certo'
-    });
+  res.status(200).json(db);
 });
 
-  router.post('/', (req, res, next) => {
-      res.status(201).send({ 
-          message: 'Usando o POST dentro da roa de produtos'
-      });
+router.post('/add', (req, res, next) => {
+  const body = req.body;
+
+  if(!body) {
+    res.status(400).end();
+    return;
+  }
+
+  db.push(body);
+  res.status(200).json(body);
+});
+
+router.delete('/:id', (req, res, next) => {
+  const id = req.params.id
+
+  let newDB = db.filter(item => {
+    if (!item[id]) {
+      return item
+    }
   });
 
-console.log('produtos');
+  db.push(body);
+  res.status(200).json(body);
+});
+
+
+router.delete('/:id', (req, res) => {
+  const id = req.params.id
+
+  let newDB = db.filter(item => {
+    if (!item[id]){
+      return item;
+    }
+  })
+
+  db = newDB;
+  res.status(200).json(newDB);
+  // return;
+})
 
 module.exports = router;
 
