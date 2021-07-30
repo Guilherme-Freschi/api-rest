5a9ca17e00540805d6225a9149ca848b430c451d
Guilherme Ciesco <guilherme.ciesco@gmail.com>
2021-07-30 18:12:19 -0300

    banco add

--- routes/produtos.js
+++ routes/produtos.js
@@ -1,26 +1,45 @@
 const express = require('express');
 const router = express.Router();
+const mysql = require('../mysql').pool;
 
-let db = [
-  {'1': {Nome: 'Cliente 1', Idade: '20'} },
-  {'2': {Nome: 'Cliente 2', Idade: '20'} },
-  {'3': {Nome: 'Cliente 3', Idade: '20'} }
-]
+// let db = [
+//   {'1': {Nome: 'Cliente 1', Idade: '20'} },
+//   {'2': {Nome: 'Cliente 2', Idade: '20'} },
+//   {'3': {Nome: 'Cliente 3', Idade: '20'} }
+// ];
 
 router.get('/', (req, res, next) => {
-  res.status(200).json(db);
-});
+  mysql.getConnection((error, conn) => {
+    if (error) { return res.status(500).send({ error: error }) }
+      conn.query(
+            'SELECT* FROM produtos;',
+            (error, resultado, fields) => {
+              if (error) { return res.status(500).send({ error: error }) }
+            });
+          })
+        });
 
 router.post('/add', (req, res, next) => {
-  const body = req.body;
-
-  if(!body) {
-    res.status(400).end();
-    return;
-  }
-
-  db.push(body);
-  res.status(200).json(body);
+  mysql.getConnection((error, conn) => {
+    if (error) { return res.status(500).send({ error: error }) }
+    conn.query(
+      'INSERT INTO produtos (nome, preco) VALUES (?,?)'
+    [req.body.name, req.body.preco],
+    (error, resultado, field) => {
+      conn.release();
+      if (error) {
+        return res.status(500).send({
+          error: error,
+          response: null
+        });
+      }
+        res.status(201).send({
+        mensagem: 'insere um produto',
+        id_produto: resultado.insertId
+        });
+      }
+    )
+  });
 });
 
 router.delete('/:id', (req, res, next) => {
